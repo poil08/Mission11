f0382551ed5af8e74078709312d2b11401fae040
poil08 <songe0324@gmail.com>
2026-04-01 01:20:28 -0600

    Getting ready to commit

--- backend/Missin11.API/obj/Debug/net10.0/Mission11.API.AssemblyInfo.cs
+++ backend/Missin11.API/obj/Debug/net10.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20c6f12ba0d4c4d41ca57a869d4c9304c89768d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a251e2a372b16f5e8946d2838825264580e2ddec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
